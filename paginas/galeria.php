2912963a91610716d41e78034b1259630dda5893
yael-cc <luiscruz_angel@hotmail.com>
2024-07-27 02:20:57 -0700

    Modificacion para que ninguna página esté en blanco.

---
+++ paginas/galeria.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cantina Azteca</title>
+    <link rel="stylesheet" href="../estilos/menu.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<?php
+    include("general.php");
+?>
+
+<body>
+    <main>
+        <!-- Lo que vayan a poner pongalo aqui -->
+    </main>
+</body>
+</html>
