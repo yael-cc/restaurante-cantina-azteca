f327b1ee6b09f2ab028550ac3abf2b3eb6a4e2e5
DJBolillo <tnagenage@gmail.com>
2024-07-29 02:05:58 -0700

    Inclusion de reservaciones.

--- paginas/reservaciones.php
+++ paginas/reservaciones.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../estilos/reservacion.css">
 </head>
 
 <?php
@@ -20,7 +21,27 @@
 
 <body>
     <main>
+        <section class="formulario-reservacion">
+            <h1>Formulario para Reservaciones</h1>
+            <hr>
+            <br><br>
+            <form action="manejo-reservaciones.php" method="post">
+                <label for="Cantidad">Cantidad de personas: </label>
+                <input type="number" name="cantidad" id="cantidad" required>
+                <br>
+                <label for="comentario">Comentarios adicionales: </label>
+                <br>
+                <textarea name="comentario" id="comentario" cols="50" rows="10" placeholder="Ingresa comentarios adicionales en caso de ser necesario.">
+                </textarea>
+                <br>
+                <label for="fecha">Fecha para la reservación:</label>
+                <br>
+                <input type="datetime-local" name="fechaR" id="fechaR" required>
+                <br>
+                <input type="submit" name="reservar" id="reservar" value="Reservar">
+            </form>
 
+        </section>
     
     </main>
 
--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,7 +1,7 @@
 <?php
     $server = 'localhost';
     $user = 'root';
-    $pass = '';
+    $pass = 'Nagetime12,';
     $db = 'restaurante';
 
     $conexion = new mysqli($server, $user, $pass, $db);
